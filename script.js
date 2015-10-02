beb2415f341cf7eb8cfcdd03e6f149e4840de7a4
Joshua Briley <josh@thebrileys.com>
2015-10-02 18:26:04 -0400

    Initial js upload.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-// prompt user for max number
+function getCount(){
+  var fbPrompt = parseInt(prompt("Enter FizzBuzz Max Number"));
+  return fbPrompt;
+}
+function FizzBuzz(FBMax){
+  for (var i = 1; i <= FBMax; i++) {
+    if ( i % 5 === 0 && i % 3 === 0){
+      $("<li class='list-group-item'>FizzBuzz</li>").appendTo($(".list-group"));
+    } else if (i % 3 === 0) {
+      $("<li class='list-group-item'>Fizz</li>").appendTo($(".list-group"));
+    } else if (i % 5 === 0) {
+      $("<li class='list-group-item'>Buzz</li>").appendTo($(".list-group"));
+    } else {
+      $("<li class='list-group-item'>" + i + "</li>").appendTo($(".list-group"));
+    }
+  }
+}
 
-// use this number as an argument for the fizzbuzz function
+FizzBuzz(getCount());
